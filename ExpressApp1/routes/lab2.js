2def656a6a27db239d2fdb7400e981ad25f0d1a8
shreyans <200393469@student.georgianc.on.ca>
2019-09-24 21:16:55 -0400

    lab2

---
+++ ExpressApp1/routes/lab2.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    if (req.query.method) {
+
+        var x = parseInt(req.query.x);
+        var y = parseInt(req.query.y);
+        if (req.query.method === 'add') {
+            var total = x + y;
+            res.send("Sample Output:" + req.query.x + " + " + req.query.y + " = " + total.toString());
+        }
+        if (req.query.method == 'subtract') {
+            var total = x - y;
+            res.send("Sample Output:" + req.query.x + " - " + req.query.y + " = " + total.toString());
+        }
+        if (req.query.method === 'multiply') {
+            var total = x * y;
+            res.send("Sample Output:" + req.query.x + " * " + req.query.y + " = " + total.toString());
+        }
+        if (req.query.method === 'divide') {
+            var total = x / y;
+            res.send("Sample Output:" + req.query.x + " / " + req.query.y + " = " + total.toString());
+        }
+    } else {
+        res.send('Math');
+    }
+});
+
+module.exports = router;
